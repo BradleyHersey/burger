2f084a746a5a05a3cd6b56457ce15086a093b216
Brad <bradleyhersey00@gmail.com>
2018-08-01 20:31:27 -0400

    8/1/18

---
+++ db/seed.sql
@@ -0,0 +1,12 @@
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Double Double');
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Thick & Hearty');
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Bacon Cheeseburger');
